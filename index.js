a1c344b503380a523428585f67dac24f42ab68cf
Magz <magdy_176@hotmail.com>
2024-07-19 11:47:00 +0100

    Cleared cache and cookies before quiting for security by default

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { app, nativeImage, BrowserWindow } = require("electron");
 const path = require("path");
 
+let win; // Define the window variable in a higher scope
+
 // Function to create the browser window
 function createWindow() {
   const iconPath = path.resolve(__dirname, "images", "icon.png");
@@ -14,27 +16,60 @@ function createWindow() {
   }
 
   // Create a new browser window
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       // Enable Node.js integration in the renderer process
       nodeIntegration: true,
       // Disable context isolation (for simplicity; generally, keep this enabled for security)
-      contextIsolation: false,
+      contextIsolation: true,
       // Path to the preload script
       preload: path.join(__dirname, "preload.js"),
       allowRunningInsecureContent: false,
       webviewTag: true, // Enable <webview> tag
+      // Enables a sandboxed renderer process, which means the web content will be run with fewer privileges.
+      sandbox: true,
+      imageAnimationPolicy: "noAnimation",
+      autoplayPolicy: "user-gesture-required",
     },
     icon: appIcon, // Use the NativeImage instance
   });
 
   // Load the index.html file into the window
   win.loadFile("renderer/index.html");
+
+  // Event listener for window close
+  win.on("close", (event) => {
+    event.preventDefault();
+    sessionCleanUp().then(() => {
+      win.destroy();
+    });
+  });
+
   // open dev tools
-  win.webContents.openDevTools();
+  //win.webContents.openDevTools();
 }
+
+const sessionCleanUp = async () => {
+  if (win) {
+    // user agent
+    const ses = win.webContents.session;
+    console.log(ses.getUserAgent());
+
+    try {
+      // Clear cache
+      await ses.clearCache();
+      console.log("Cache cleared");
+
+      // Clear all storage data
+      await ses.clearStorageData();
+      console.log("Storage data cleared");
+    } catch (error) {
+      console.error("Error during session cleanup:", error);
+    }
+  }
+};
 
 // Event handler for when Electron has finished initializing
 app.whenReady().then(createWindow);
@@ -43,7 +78,9 @@ app.whenReady().then(createWindow);
 app.on("window-all-closed", () => {
   // Quit the application if not on macOS
   if (process.platform !== "darwin") {
-    app.quit();
+    sessionCleanUp().then(() => {
+      app.quit();
+    });
   }
 });
 
@@ -53,4 +90,12 @@ app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+// Perform session cleanup when the application is quitting
+app.on("before-quit", (event) => {
+  event.preventDefault();
+  sessionCleanUp().then(() => {
+    app.quit();
+  });
 });
